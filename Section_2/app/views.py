6fe4dd36a503f72f25d0147bad8ff879ee2b115e
seifzogheibi <146748894+seifzogheibi@users.noreply.github.com>
2024-10-16 21:30:20 +0000

    cw1 setup

--- Section_2/app/views.py
+++ Section_2/app/views.py
@@ -1,5 +1,29 @@
+from flask import render_template, flash
 from app import app
+from .forms import CalculatorForm
 
 @app.route('/')
 def index():
-    return "Hello World!!!"
+    user = {'name': 'Homer Simpson'}
+    return render_template('index.html',
+                           #title='Simple template example',
+                           user=user)
+
+@app.route('/fruit')
+def displayFruit():
+    fruits = ["Apple", "Banana", "Orange", "Kiwi"]
+    return render_template("fruit.html",fruits=fruits)
+
+@app.route('/calculator', methods=['GET', 'POST'])
+def calculator():
+    form = CalculatorForm()
+    if form.validate_on_submit():
+        flash('Succesfully received form data. %s + %s  = %s'%(form.number1.data, form.number2.data, form.number1.data+form.number2.data))
+    return render_template('calculator.html',
+                           title='Calculator',
+                           form=form)
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    home={'description':'Welcome to this application. Please select Calculator to calculate two numbers.'}
+    return render_template('home.html', title='Home', home=home)

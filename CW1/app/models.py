1de64295f557bfb993662cedc7eb4964fc0439da
seifzogheibi <146748894+seifzogheibi@users.noreply.github.com>
2024-10-29 17:40:28 +0000

    final final

--- CW1/app/models.py
+++ CW1/app/models.py
@@ -1,8 +1,8 @@
 from app import db
 
 
-# Initializing all the data types that will be
-# used to create the assessment tracker
+# Creating assessment models that will store 
+# the data in the database
 class Assessment(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
